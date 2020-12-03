1e61e56d9f86e45f0e197c524f42a5e1f1cc966a
Marcel Walk <walk.marcel.97@googlemail.com>
2020-12-03 15:26:50 +0100

    Finished Day 3

--- DayThree.cs
+++ DayThree.cs
@@ -4,35 +4,33 @@ namespace AdventOfCode
 {
     public class DayThree
     {
-        public static int Day3_P1(char[][] map)
+        public static long Day3_P1(char[][] map,int right, int down)
         {
-            /*
-            Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
-            */
             var x = 0;
-            var treeCounter = 0;
-            for (var y = 0; y < map.Length; y++)
-                if (x < map[y].Length)
-                {
-                    Console.Write($"{y}, {x} | ");
-                    switch (map[y][x])
-                    {
-                        case '.':
-                            Console.WriteLine("Open");
-                            break;
-                        case '#':
-                            Console.WriteLine("Tree");
-                            treeCounter++;
-                            break;
-                    }
 
-                    x += 3;
-                }
-                else
+            //counting all the trees
+            long treeCounter = 0;
+
+            //You start on the open square (.) in the top-left corner
+            for (var y = 0; y < map.Length; y+= down) // down 1
+            {
+                Console.Write($"{y}, {x} | ");
+                switch (map[y][x])
                 {
-                    x = 3;
+                    case '.':
+                        Console.WriteLine("Open");
+                        break;
+                    case '#':
+                        Console.WriteLine("Tree");
+                        treeCounter++;
+                        break;
                 }
 
+                //right 3
+                x += right;
+                if (x >= map[y].Length)
+                    x -= map[y].Length;
+            }
             return treeCounter;
         }
     }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,14 @@ namespace AdventOfCode
             var input = File.ReadAllLines("./files/Day3.input");
             var inputArray = input.Select(x => x.ToCharArray()).ToArray();
 
-            Console.WriteLine(DayThree.Day3_P1(inputArray));
+            var p1 = DayThree.Day3_P1(inputArray, 1, 1);
+            var p2 = DayThree.Day3_P1(inputArray, 3, 1);
+            var p3 = DayThree.Day3_P1(inputArray, 5, 1);
+            var p4 = DayThree.Day3_P1(inputArray, 7, 1);
+            var p5 = DayThree.Day3_P1(inputArray, 1, 2);
+
+            long res = p1 * p2 * p3 * p4 * p5;
+
             Console.ReadLine();
         }
     }
