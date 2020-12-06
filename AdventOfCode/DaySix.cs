cc9f83065d166fc0c98806f1097c54b68a86f3c1
Marcel Walk <walk.marcel.97@googlemail.com>
2020-12-06 10:07:16 +0100

    Added Day 6 and applied gitigore

---
+++ AdventOfCode/DaySix.cs
@@ -0,0 +1,12 @@
+namespace AdventOfCode
+{
+    public class DaySix
+    {
+        public static int Day6_P1(string[] inp)
+        {
+        }
+        public static int Day6_P2(string[] inp)
+        {
+        }
+    }
+}
